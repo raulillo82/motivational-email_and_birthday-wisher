2b356250c6248e91d05519a1954fa5b4edd5b9b9
Raúl Osuna <rosuna@suse.com>
2023-11-02 01:41:43 +0100

    Move sensible data into specific authentication file with variables

--- birthday_wisher.py
+++ birthday_wisher.py
@@ -21,21 +21,11 @@ if now_tuple in birthdays_dict:
 
 # 4. Send the letter generated in step 3 to that person's email address.
 
-    PASSWORD_FILE = "./password"
-    EMAIL_FILE = "./email"
     SMTP_SERVER = "smtp.gmail.com"
 
-    try:
-        with open(EMAIL_FILE) as email_file:
-            email = email_file.readline()
-        with open(PASSWORD_FILE) as password_file:
-            password = password_file.readline()
-    except FileNotFoundError:
-        print(f"File {PASSWORD_FILE} not found! Terminating the program.")
-    else:
-        with smtplib.SMTP(SMTP_SERVER) as connection:
-            connection.starttls()
-            connection.login(user=email, password=password)
-            name = person["name"]
-            msg = f"Subject:Happy birthday, {name}!\n\n{content}"
-            connection.sendmail(from_addr=email, to_addrs=email, msg=msg)
+    with smtplib.SMTP(SMTP_SERVER) as connection:
+        connection.starttls()
+        connection.login(user=email, password=password)
+        name = person["name"]
+        msg = f"Subject:Happy birthday, {name}!\n\n{content}"
+        connection.sendmail(from_addr=email, to_addrs=email, msg=msg)
--- motivational_mail.py
+++ motivational_mail.py
@@ -1,9 +1,9 @@
 import smtplib
 import datetime as dt
 from random import choice
+from auth import *
+
 QUOTES = "./quotes.txt"
-PASSWORD_FILE = "./password"
-EMAIL_FILE = "./email"
 SMTP_SERVER = "smtp.gmail.com"
 
 try:
@@ -13,18 +13,10 @@ except FileNotFoundError:
     print(f"File {QUOTES} not found! Terminating the program.")
 else:
     if dt.datetime.now().weekday() == 1: #Check for Mondays
-        try:
-            with open(EMAIL_FILE) as email_file:
-                email = email_file.readline()
-            with open(PASSWORD_FILE) as password_file:
-                password = password_file.readline()
-        except FileNotFoundError:
-            print(f"File {PASSWORD_FILE} not found! Terminating the program.")
-        else:
-            with smtplib.SMTP(SMTP_SERVER) as connection:
-                connection.starttls()
-                connection.login(user=email, password=password)
-                quote = choice(quotes_list)
-                msg = "Subject:Motivational mail of the week\n\n" + quote
-                connection.sendmail(from_addr=email, to_addrs=email,
-                                    msg=msg)
+        with smtplib.SMTP(SMTP_SERVER) as connection:
+            connection.starttls()
+            connection.login(user=email, password=password)
+            quote = choice(quotes_list)
+            msg = "Subject:Motivational mail of the week\n\n" + quote
+            connection.sendmail(from_addr=email, to_addrs=email,
+                                msg=msg)
